10d1a68d63f4917e62597927847ec826f2de58db
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-19 09:41:36 +0530

    basic update

--- src/server.js
+++ src/server.js
@@ -4,47 +4,52 @@ const { Server } = require("socket.io");
 const Redis = require("ioredis");
 const bodyParser = require("body-parser");
 
-const redisCache = new Redis({
-  host: "redis",
-  port: 6379,
-});
-
 const app = express();
-
 app.use(bodyParser.json());
-
 const httpServer = createServer(app);
+
+const redisCache = new Redis();
+
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5500",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  console.log("A user connected " + socket.id);
+  socket.on("setUserId", (userId) => {
+    console.log("Setting user id to connection id", userId, socket.id);
+    redisCache.set(userId, socket.id);
   });
 
   socket.on("getConnectionId", async (userId) => {
     const connId = await redisCache.get(userId);
-    console.log("Getting connection id for user", userId, connId);
+    console.log("Getting connection id for user id", userId, connId);
     socket.emit("connectionId", connId);
+    const everything = await redisCache.keys("*");
+
+    console.log(everything);
   });
 });
 
 app.post("/sendPayload", async (req, res) => {
+  console.log(req.body);
+  const { userId, payload } = req.body;
   if (!userId || !payload) {
-    return res.sendStatus(400).send("Bad request");
+    return res.status(400).send("Invalid request");
   }
-
   const socketId = await redisCache.get(userId);
-  if (!socketId) {
-    return res.sendStatus(404).send("Bad request");
+
+  if (socketId) {
+    io.to(socketId).emit("submissionPayloadResponse", payload);
+    return res.send("Payload sent successfully");
+  } else {
+    return res.status(404).send("User not connected");
   }
 });
 
-httpServer.listen(3000, () => {
-  console.log("listening on *:3000");
+httpServer.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
